8fa3bf2ef7c8941e375ca82ef3860e9ba6d84a6b
Flavianthepavian <flavianzuellig@hotmail.com>
2024-07-05 19:42:06 -0400

    remove old design and add simple header

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Home from "./sites/Home/Home.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Frame from "./Frame/Frame.tsx";
 
-const router = createBrowserRouter([{path: "/", element: <Home/>}])
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Frame />
   </React.StrictMode>,
-)
+);
