f06f99909a9b7d8604293035c918b970eaad1f24
Flavianthepavian <flavianzuellig@hotmail.com>
2024-11-18 14:42:45 +0100

    add tech stack to each project

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -11,47 +11,78 @@ export default function Projects({
         <div className={className + " " + styles.container}>
             <h1 className={styles.title}>Recent projects</h1>
             <div className={styles.projects}>
-                {[
-                    {
-                        title: "School App",
-                        description:
-                            "In secondary school, we had a very old and poor quality tool where we could see our grades, timetables etc. To fix this, I created a Node.js API that crawled the students' data and a React Native mobile app to go with it.",
-                    },
-                    {
-                        title: "Compiler",
-                        description:
-                            "As a school project, I created a compiler in Typescript that compiled 'EmojiDo', a programming language I invented that only uses Emojis, to x86 Assembly. Along the way, I wrote a small public documentation for the language.",
-                        href: "https://emojido.flavianz.ch",
-                    },
-                    {
-                        title: "Chess Engine",
-                        description:
-                            "Right now, I am trying to build a UCI chess engine called 'Cobalt' in Rust that can beat me or any other average to good player in a game of chess.",
-                        href: "https://www.github.com/flavianz/cobalt",
-                    },
-                ].map((project, id) => {
-                    return (
-                        <a
-                            href={project.href}
-                            target={"_blank"}
-                            key={id}
-                            className={
-                                styles.projectContainer +
-                                (project.href
-                                    ? " " + styles.projectClickable
-                                    : "")
-                            }
-                        >
-                            <h3 className={styles.projectHeader}>
-                                {project.title}
-                            </h3>
-                            <p className={styles.projectDescription}>
-                                {project.description}
-                            </p>
-                        </a>
-                    );
-                })}
+                <Item
+                    title={"School App"}
+                    description={
+                        "In secondary school, we had a very old and poor quality tool where we could see our grades, timetables etc. To fix this, I created a Node.js API that crawled the students' data and a React Native mobile app to go with it."
+                    }
+                    href={""}
+                    stack={[
+                        "React Native",
+                        "Typescript",
+                        "Firebase",
+                        "Node.js",
+                        "Expo",
+                    ]}
+                />
+                <Item
+                    title={"Compiler"}
+                    description={
+                        "As a school project, I created a compiler in Typescript that compiled 'EmojiDo', a programming language I invented that only uses Emojis, to x86 Assembly. Along the way, I wrote a small public documentation for the language."
+                    }
+                    href={"https://emojido.flavianz.ch"}
+                    stack={["Typescript", "ASM x86", "React", "MDX", "Vite"]}
+                />
+                <Item
+                    title={"Song Guessing Game"}
+                    description={
+                        "Right now, I am building a small multiplayer game where you have to guess the song based on its lyrics. "
+                    }
+                    href={"https://www.github.com/flavianz/songy"}
+                    stack={["React", "Vite", "Firebase", "Node.js"]}
+                />
             </div>
         </div>
+    );
+}
+
+function Item({
+    description,
+    title,
+    href,
+    stack,
+}: {
+    description: string;
+    title: string;
+    href: string | undefined;
+    stack: string[];
+}) {
+    return (
+        <a
+            href={href}
+            target={"_blank"}
+            className={
+                styles.projectContainer +
+                (href ? " " + styles.projectClickable : "")
+            }
+        >
+            <div>
+                <h3 className={styles.projectHeader}>{title}</h3>
+                <p className={styles.projectDescription}>{description}</p>
+            </div>
+            <div id={styles.stackBubbleContainer}>
+                {stack.map((text, key) => (
+                    <Bubble key={key} text={text} />
+                ))}
+            </div>
+        </a>
+    );
+}
+
+function Bubble({ text, key }: { text: string; key: number }) {
+    return (
+        <p key={key} className={styles.stackBubble}>
+            {text}
+        </p>
     );
 }
